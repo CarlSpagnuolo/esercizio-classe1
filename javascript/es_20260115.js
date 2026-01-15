014fb7e81d2e4f56d64e2d66b2d5c8278132a2c1
Carlo Spagnuolo <carloppannolo@hotmail.it>
2026-01-15 20:04:18 +0100

    app todo list

---
+++ javascript/es_20260115.js
@@ -0,0 +1,12 @@
+// QUESTA è UN'APP TODO LIST
+// CREA UNA FUNZIONE PER AGGIUNGERE
+// CREA UNA FUNZIONE PER ELIMINARE
+// CREA UNA FUNZIONE PER MODIFICARE
+// CREA UNA FUNZIONE PER STAMPARE
+// 1:ID UNIVOCO, 2:NOME ATTIVITà, 3:DESCRIZIONE ATTIVITà, 4:STATO;
+
+const todo = [
+  { id: null, nomeAttivita: null, descrizioneAttivita: null, stato: false },
+];
+
+function toAdd(todo) {}
